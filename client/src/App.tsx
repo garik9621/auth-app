c6a7541405b5e44e1dfb69ef2b4f42b96fa446cb
Igor Seliverstov <seliverstov@logema.org>
2022-07-19 11:12:01 +0300

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css';
+import { Auth } from './components/Auth/Auth';
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header"></header>
+
+			<main>
+				<Auth />
+			</main>
+
+			<footer></footer>
+		</div>
+	);
+}
+
+export default App;
---
+++ client/src/components/Auth/Auth.tsx
@@ -0,0 +1,60 @@
+import React, { ChangeEventHandler, useState } from 'react';
+import './Auth.css';
+import { TextField, Button } from '@mui/material';
+import axios from 'axios';
+
+export const Auth: React.FC = () => {
+	const [login, setLogin] = useState('');
+	const [password, setPassword] = useState('');
+
+	const submitHandler = (e: React.FormEvent) => {
+		e.preventDefault();
+
+		axios.get('/login', {
+			params: {
+				login,
+				password,
+			},
+		});
+	};
+
+	const onLoginChange = (
+		e: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>
+	) => {
+		setLogin(e.currentTarget.value);
+	};
+
+	const onPasswordChange = (
+		e: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>
+	) => {
+		setPassword(e.currentTarget.value);
+	};
+
+	return (
+		<form className="Auth" onSubmit={(e) => submitHandler(e)}>
+			<TextField
+				label="Login"
+				type="text"
+				name="login"
+				variant="outlined"
+				margin="dense"
+				value={login}
+				onChange={(e) => onLoginChange(e)}
+			/>
+
+			<TextField
+				label="Password"
+				type="password"
+				name="password"
+				variant="outlined"
+				margin="dense"
+				value={password}
+				onChange={(e) => onPasswordChange(e)}
+			/>
+
+			<Button variant="contained" type="submit">
+				Submit
+			</Button>
+		</form>
+	);
+};
